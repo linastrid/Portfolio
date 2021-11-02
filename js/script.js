ce2985c0aae655d334011ea467014a82d1e46812
justin <jusin4053@gmail.com>
2021-11-02 16:50:42 +0800

    index RWD

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// 設定兩個 cookie 叫 test1, test2
+// 在瀏覽器關閉後會自動被刪除
+document.cookie = 'test1=Hello'
+document.cookie = 'test2=World'
+
+// 設定一個 username cookie 裡面的值是 Mike，儲存一個月
+document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+
+$(document).ready(function () {
+    // Show or hide the sticky footer button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 768) {
+            $('#button').fadeIn(200);
+        } else {
+            $('#button').fadeOut(200);
+        }
+    });
+
+    // Animate the scroll to top
+    $('#button').click(function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({ scrollTop: 0 }, 300);
+    })
+});
+/*改變原始寬度（0）*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "151px";
+}
+/*恢復原始寬度0*/
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
